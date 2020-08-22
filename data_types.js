71b5623348b2d09ebf99918a6b83f78a6f90014b
itexnoplus <itexnoplusuz@mail.ru>
2020-08-22 17:25:51 +0500

    course

---
+++ data_types.js
@@ -0,0 +1,16 @@
+// PRIMITIVE DATA TYPES
+let x = 10; // number
+let text = "matn"; // string
+let sym = Symbol(); // Symbol (ES6)
+let hasKids = true; // boolean
+let und;
+// REFERENCE DATA TYPES
+let hobbies = ["Music", "Football", "Study"]; // Array
+
+let student = {
+  name: "Jamshid",
+  age: 20,
+  job: "dasturchi"
+} // Object
+
+console.log("Assalomu alaykum " + student.name);
